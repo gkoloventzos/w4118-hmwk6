e26d7276b91bd69fd2510fb6b04e9e84d288f9f9
Andrei Papancea <alp2200@columbia.edu>
2014-11-26 15:16:24 -0500

    defined get_gps_location syscall

---
+++ flo-kernel/include/linux/gps.h
@@ -0,0 +1,10 @@
+/*
+ * E. Atlidakis, G. Koloventzos, A. Papancea
+ * UNI: ea2615, gk2409, alp2200
+ * Last updated: 11/26/2014
+ */
+struct gps_location {
+	double latitude;
+	double longitude;
+	float  accuracy;  /* in meters */
+};
