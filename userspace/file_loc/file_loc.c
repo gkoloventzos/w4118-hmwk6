7fec339b8352bd213b4e259e699b7a057af3c784
Andrei Papancea <alp2200@columbia.edu>
2014-12-03 14:10:45 -0500

    almost done with file_loc utility

--- userspace/file_loc/file_loc.c
+++ userspace/file_loc/file_loc.c
@@ -1,6 +1,54 @@
+/*
+ * file_loc.c
+ *
+ * Columbia University
+ * COMS W4118 Fall 2014
+ * Homework 6
+ *
+ * E. Atlidakis, G. Koloventzos, A. Papancea
+ * UNI: ea2615, gk2409, alp2200
+ * Last updated: 12/03/2014
+ *
+ */
 #include "file_loc.h"
+
+static void print_location(struct gps_location *loc, int age)
+{
+	char *url = "https://maps.google.com/maps?q=";
+
+	printf("latitude:\t-73.23456\n");
+	printf("longitude:\t40.34500\n");
+	printf("accuracy:\t20.00000\n");
+	printf("data age:\t%d\n", age);
+	printf("maps url:\t%s12.2+14.3\n", url);
+}
 
 int main(int argc, char **argv)
 {
+	int rval;
+	char *path;
+	struct gps_location *loc;
+
+	if (argc != 2) {
+		printf("Usage: %s {file path}\n", argv[0]);
+		return -1;
+	}
+
+	path = argv[1];
+
+	loc = malloc(sizeof(struct gps_location));
+	if (loc == NULL) {
+		perror("malloc");
+		return -1;
+	}
+
+	rval = get_gps_location(path, loc);
+	if (rval < 0) {
+		perror("get_gps_location");
+		return -1;
+	}
+
+	print_location(loc, rval);
+
 	return 0;
 }
--- userspace/file_loc/file_loc.h
+++ userspace/file_loc/file_loc.h
@@ -5,14 +5,28 @@
  *
  * Columbia University
  * COMS W4118 Fall 2014
- * Homework 6 - Geo Tagged File System
+ * Homework 6
+ *
+ * E. Atlidakis, G. Koloventzos, A. Papancea
+ * UNI: ea2615, gk2409, alp2200
+ * Last updated: 12/03/2014
+ *
  */
+#include <errno.h>
+#include <stdio.h>
+#include <errno.h>
+#include <string.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/stat.h>
+#include <sys/ioctl.h>
+#include <sys/types.h>
 #include <sys/syscall.h>
 
 struct gps_location {
 	double latitude;
 	double longitude;
-	float  accuracy; 
+	float  accuracy;
 };
 
 static inline int get_gps_location(const char *pathname,
