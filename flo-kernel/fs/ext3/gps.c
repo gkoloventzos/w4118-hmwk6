7709916d49a82d829c3ce6d02d5c58869a8d4d1c
Vaggelis Atlidakis <vatlidak@cs.columbia.edu>
2014-12-04 22:30:30 -0500

    Update ext3_get/set_gps_location functions

--- flo-kernel/fs/ext3/gps.c
+++ flo-kernel/fs/ext3/gps.c
@@ -2,7 +2,6 @@
 #include <linux/gps.h>
 
 /*
- *
  * NOTE: Caller should hold the inode lock
  */
 int ext3_set_gps_location(struct inode *inode)
@@ -12,17 +11,10 @@ int ext3_set_gps_location(struct inode *inode)
 
 	get_location(&local);
 
-	printk(KERN_ERR "\n\nFUCKING SERIOUS??????\n\n\n");
-
 	ei = EXT3_I(inode);
-	ei->i_latitude = *((unsigned long long *) &local.latitude);
-	ei->i_longitude = *((unsigned long long *) &local.longitude);
-	ei->i_accuracy = *((unsigned long *) &local.accuracy);
-
-	printk(KERN_ERR "storing lat=%ld, lng=%ld, acc=%ld\n",
-			*((long int *)&ei->i_latitude),
-			*((long int *)&ei->i_longitude),
-			*((long int *)&ei->i_accuracy));
+	memcpy((void* )&ei->i_latitude, (void *)&local.latitude, sizeof(unsigned long long));
+	memcpy(&ei->i_longitude, &local.longitude, sizeof(unsigned long long));
+	memcpy(&ei->i_accuracy, &local.accuracy, sizeof(unsigned int));
 
 	return 0;
 }
@@ -38,12 +30,12 @@ int ext3_get_gps_location(struct inode *inode, struct gps_location *location)
 
 	spin_lock(&inode->i_lock);
 	ei = EXT3_I(inode);
-	local.latitude = *((double *)&ei->i_latitude);
-	local.longitude = *((double *)&ei->i_longitude);
-	local.accuracy = *((float *)&ei->i_accuracy);
+	memcpy(&local.latitude, &ei->i_latitude, sizeof(unsigned long long));
+	memcpy(&local.longitude, &ei->i_longitude, sizeof(unsigned long long));
+	memcpy(&local.accuracy, &ei->i_accuracy, sizeof(unsigned int));
 	spin_unlock(&inode->i_lock);
 
-	memcpy(&location, &local, sizeof(local));
+	memcpy(location, &local, sizeof(local));
 
 	return 0;
 }
