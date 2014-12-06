6330f4692925ea1cafa2c344ddc0b2248edda4fd
Andrei Papancea <alp2200@columbia.edu>
2014-12-06 13:45:15 -0500

    keeping track of i_coord_age

--- flo-kernel/fs/ext3/gps.c
+++ flo-kernel/fs/ext3/gps.c
@@ -6,6 +6,7 @@
  *
  * COMS W4118 Fall 2014, Columbia University
  */
+#include <linux/time.h>
 #include <linux/gps.h>
 #include "ext3.h"
 
@@ -18,6 +19,7 @@
  */
 int ext3_set_gps_location(struct inode *inode)
 {
+	int coord_age;
 	struct gps_location local;
 	struct ext3_inode_info *ei = EXT3_I(inode);
 
@@ -28,10 +30,12 @@ int ext3_set_gps_location(struct inode *inode)
 	 * exposed from /kernel/gps.c layer.
 	 */
 	get_location(&local);
+	coord_age = CURRENT_TIME_SEC-local.coord_age;
 
 	memcpy(&ei->i_latitude, &local.latitude, sizeof(unsigned long long));
 	memcpy(&ei->i_longitude, &local.longitude, sizeof(unsigned long long));
 	memcpy(&ei->i_accuracy, &local.accuracy, sizeof(unsigned int));
+	memcpy(&ei->i_coord_age, &coord_age, sizeof(int));
 
 	return 0;
 }
@@ -54,6 +58,7 @@ int ext3_get_gps_location(struct inode *inode, struct gps_location *location)
 	memcpy(&local.latitude, &ei->i_latitude, sizeof(unsigned long long));
 	memcpy(&local.longitude, &ei->i_longitude, sizeof(unsigned long long));
 	memcpy(&local.accuracy, &ei->i_accuracy, sizeof(unsigned int));
+	memcpy(&local.coord_age, &ei->i_coord_age, sizeof(int));
 	memcpy(location, &local, sizeof(local));
 
 	return 0;
